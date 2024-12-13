86d9281623732fbb6a67f9f18afcbe9629d46468
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-12-13 21:21:03 +0000

    Even smaller

--- day13.py
+++ day13.py
@@ -1,15 +1,12 @@
 import util, parse
 def solve(a, b, c, d, e, f):
     det, x, y = a*d-c*b, d*e-c*f, a*f-b*e
-    if x % det == 0 and y % det == 0:
-        return 3 * x // det + y // det
+    return 3 * x // det + y // det if x % det == 0 and y % det == 0 else 0
 
 count1 = count2 = 0
 pattern = "Button A: X+{:d}, Y+{:d}\nButton B: X+{:d}, Y+{:d}\nPrize: X={:d}, Y={:d}"
 for machine in util.get_input().split("\n\n"):
     a, b, c, d, e, f = parse.parse(pattern, machine).fixed
-    if ans := solve(a, b, c, d, e, f):
-        count1 += ans
-    if ans := solve(a, b, c, d, e + 10**13, f + 10**13):
-        count2 += ans
+    count1 += solve(a, b, c, d, e, f)
+    count2 += solve(a, b, c, d, e + 10**13, f + 10**13)
 print(count1, count2)
