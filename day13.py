2801fb7caac1dc0c7daf7fdbc36374bd568bfc84
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-12-13 21:06:36 +0000

    Reduce line count

--- day13.py
+++ day13.py
@@ -1,17 +1,16 @@
 import util
 import parse
-data = util.get_input()
 
 def solve(a, b, c, d, e, f):
-    det = (a*d-c*b)
-    x = (d*e-c*f)
-    y = (a*f-b*e)
+    det = a*d-c*b
+    x = d*e-c*f
+    y = a*f-b*e
     if x % det == 0 and y % det == 0:
         return 3 * x // det + y // det
 
 count1 = count2 = 0
-for machine in data.split("\n\n"):
-    pattern = "Button A: X+{:d}, Y+{:d}\nButton B: X+{:d}, Y+{:d}\nPrize: X={:d}, Y={:d}"
+pattern = "Button A: X+{:d}, Y+{:d}\nButton B: X+{:d}, Y+{:d}\nPrize: X={:d}, Y={:d}"
+for machine in util.get_input().split("\n\n"):
     a, b, c, d, e, f = parse.parse(pattern, machine).fixed
     if ans := solve(a, b, c, d, e, f):
         count1 += ans
